cfa0beb77a083ca2f07b74c499be7f7f5d822f73
Your Name <you@example.com>
2017-11-01 13:23:18 +0300

    Adding automatic Who list update

--- GuildInvite.lua
+++ GuildInvite.lua
@@ -1,6 +1,8 @@
-debug = false
+Debug = false
 GuildInvite_database = GuildInvite_database or {}
-InvMessage = "inv to guild"
+InvMessage = "inv me to guild"
+WaitingForWhoUpdate = false
+WhoUpdateInterval = 60*5 -- 5 minutes
 
 function msg(msg)
     if( msg == nil ) then
@@ -14,37 +16,50 @@ function housekeeper()
         -- 30 days = 60*60*24*30
         if difftime(time(),value) > 60*60*24*30 then
             GuildInvite_database[index] = nil
-            if debug == true then msg("GuildInvite_database will be removes: "..index) end
+            if Debug == true then msg("GuildInvite_database will be removes: "..index) end
         end
+    end
+end
+function whoupdateloop(self, sinceLastUpdate, ...)
+    self.sinceLastUpdate = (self.sinceLastUpdate or 0) + sinceLastUpdate
+    if ( self.sinceLastUpdate > WhoUpdateInterval ) then
+        if Debug == true then msg("Updating Who list...") end
+        SendWho('g-""')
+        WaitingForWhoUpdate = true
+        self.sinceLastUpdate = 0
     end
 end
 
 local frame = CreateFrame("FRAME")
+local frame2 = CreateFrame("FRAME")
 frame:RegisterEvent("ADDON_LOADED")
 frame:RegisterEvent("CHAT_MSG_WHISPER")
 frame:RegisterEvent("WHO_LIST_UPDATE")
 frame:RegisterEvent("GUILD_ROSTER_UPDATE")
 local function eventHandler(self, event, ...)
-    if event == "WHO_LIST_UPDATE" then
+    if event == "WHO_LIST_UPDATE" and WaitingForWhoUpdate then
+        SetCVar("Sound_EnableSFX",0) -- disable Hide() sound
+        FriendsFrame:Hide()
+        SetCVar("Sound_EnableSFX",1) -- enable Hide() sound
         local numWhos, totalCount = GetNumWhoResults();
         for i = 1,numWhos do
             local name, guild = GetWhoInfo(i)
             -- send msg only to guys not in blacklist and with no guild
             if not GuildInvite_database[name] and guild == '' then
-                SendChatMessage(string.format("Hey, %s. I want to invite you to our guild <%s>", name, GetGuildInfo("player")), "WHISPER", nil, name)
-                SendChatMessage("You can autojoin by whisper me this message:", "WHISPER", nil, name)
-                SendChatMessage(InvMessage, "WHISPER", nil, name)
+                SendChatMessage(string.format("Hey, %s. We are pleased to invite you to <%s>", name, GetGuildInfo("player")), "WHISPER", nil, name)
+                SendChatMessage(string.format("You will receive guild invite if you whisper me this message: '%s'", InvMessage), "WHISPER", nil, name)
                 -- msg(string.format("Hey, %s\nI want to invite you to our guild %s\nYou can autojoin by whisper me with this message:\n%s", name,GetGuildInfo("player"),InvMessage))
                 GuildInvite_database[name] = time()
             end
         end
+        WaitingForWhoUpdate = false
     elseif event == "CHAT_MSG_WHISPER" then
-        local name,msg = arg2,arg1
-        if msg == InvMessage and CanGuildInvite() then
-            if debug == true then msg("Sending guild invitation to "..name) end
+        local name,MsgContent = arg2,arg1
+        if MsgContent == InvMessage and CanGuildInvite() then
+            if Debug == true then msg("Sending guild invitation to "..name) end
             GuildInvite(name)
         elseif not CanGuildInvite() then
-            if debug == true then msg("You cannot invite players to guild") end
+            if Debug == true then msg("You cannot invite players to guild") end
         end
     elseif event == "ADDON_LOADED" then
         housekeeper()
@@ -55,8 +70,9 @@ local function eventHandler(self, event, ...)
     --     for i = 1,NumGuildMembers do
     --         local name = GetGuildRosterInfo(i)
     --         GuildInvite_database[name] = 999999999999
-    --         if debug == true then msg("adding "..name.." to ignore list") end
+    --         if Debug == true then msg("adding "..name.." to ignore list") end
     --     end
     -- end
 end
 frame:SetScript("OnEvent", eventHandler)
+frame2:SetScript("OnUpdate", whoupdateloop)
